8082f2fc5be6932b458c8c4bfc2eaf44eb32f33c
Yerzhan Orazayev <yorazaye@e1z3r6p23.42.us.org>
2019-10-11 17:24:26 -0700

    Fillit updated

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: yorazaye <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/10/08 09:39:05 by yorazaye          #+#    #+#              #
-#    Updated: 2019/10/08 10:12:53 by yorazaye         ###   ########.fr        #
+#    Updated: 2019/10/11 12:21:03 by yorazaye         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,7 @@ NAME = fillit
 
 F_C = $(addsuffix .c, $(NAME))
 
-F_O = $(addsuffix .o, $(F_O))
+F_O = $(addsuffix .o, $(NAME))
 
 F_I = libft/includes
 
@@ -23,11 +23,12 @@ FLAGS = -Wall -Wextra -Werror -L libft -lft -g
 all: $(NAME)
 
 $(NAME):
-	gcc -o $(NAME) $(F_C) -I $(F_I) $(FLAGS)
+	gcc -o $(NAME) src/$(F_C) -I $(F_I) $(FLAGS)
 
 clean:
 	rm -f $(F_O)
 
 fclean: clean
+	rm -f $(NAME)
 
 re: fclean all
--- src/fillit.c
+++ src/fillit.c
@@ -6,11 +6,12 @@
 /*   By: yorazaye <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 10:09:50 by yorazaye          #+#    #+#             */
-/*   Updated: 2019/10/10 10:50:58 by yorazaye         ###   ########.fr       */
+/*   Updated: 2019/10/11 13:51:42 by yorazaye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "get_next_line.h"
 
 void	fillit(char *str)
 {
@@ -18,9 +19,49 @@ void	fillit(char *str)
 	ft_putchar('\n');
 }
 
-int		main(int ac, char **av)
+static void		tet_list(char *str)
+{
+	t_list	*tetrimon;
+
+	tetrimon = ft_lstnew(str, 16);
+}
+
+static int		check_file(int counter, char *line)
+{
+	if (counter == 5)
+	{
+		counter = 0;
+		if (ft_strcmp(line, "") != 0)
+			return (-1);
+	}
+	else if (ft_strlen(line) != 4)
+		return (-1);
+	return (1);
+}
+
+static int		get_tet(char *str)
+{
+	int		fd;
+	char	*line;
+	int		rgnl;
+	int		counter;
+
+	fd = open(str, O_RDONLY);
+	line = ft_strnew(16);
+	counter = 0; 
+	while ((rgnl = get_next_line(fd, &line)) > 0)
+	{
+		if (check_file(++counter, line) == -1)
+			return (-1);
+	}
+	return (1);
+}
+
+int				main(int ac, char **av)
 {
 	if (ac == 2)
-		fillit(av[1]);
+		ft_putnbr(get_tet(av[1]));
+	else
+		ft_putstr("./fillit [should be used this way]\n");
 	return (0);
 }
